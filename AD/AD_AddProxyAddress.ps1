6bb3624279d117abdd2048da6ff7e7b4a1cdae88
Graham Williams <gray@live.co.uk>
2016-08-09 10:38:50 +0100

    Adds comments to AD_AddProxyAddress
    Adds my file description and basic instructions

--- AD/AD_AddProxyAddress.ps1
+++ AD/AD_AddProxyAddress.ps1
@@ -1,4 +1,17 @@
-﻿$username = 'hazel.nutt'
+﻿###############################################################################
+#
+# Title: Microsoft Active Directory Proxy Addresses 
+# Author: Graham Williams - http://github.com/GrayW
+# Date: 09/08/2016
+#
+# Needs to be run in Powershell Prompt with Administrative permissions
+# Update $username to the user logon name
+# Update $proxyAddresses to required address
+# Can use multiple addresses separated by ,
+#
+###############################################################################
+
+$username = 'hazel.nutt'
 $proxyAddresses = "$username@domain.com"
 Set-ADUser -Identity $username -Add @{
     'proxyAddresses' = $proxyAddresses | % { "smtp:$_" }
